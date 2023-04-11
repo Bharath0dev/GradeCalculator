07ac3800e94ea1d58ef3cbd17f79fffb9e59adad
BHARATH VENUKATHALLA <130401372+Bharath0dev@users.noreply.github.com>
2023-04-11 13:09:58 +0530

    Add files via upload

---
+++ gc.js
@@ -0,0 +1,321 @@
+function calculate(){
+
+    //grades
+    //let o=10, a=9, b=8, c=7, d=6, e=5, f;
+    let s1= document.getElementById("sub1").value;
+    if(s1=='O')
+    {
+        var a=10;
+    }else if(s1=='A')
+    {
+        var a=9;
+    }else if(s1=='B')
+    {
+        var a=8;
+    }else if(s1=='C')
+    {
+        var a=7;
+    }else if(s1=='D')
+    {
+        var a=6;
+    }else if(s1=='E')
+    {
+        var a=5;
+    }else if(s1=='')
+    {
+        var a=0;
+    }else
+    {
+        document.getElementById("Err").innerHTML="Enter a Valid Value";
+    }
+
+
+    let s2= document.getElementById("sub2").value;
+    if(s2=='O')
+    {
+        var b=10;
+    }else if(s2=='A')
+    {
+        var b=9;
+    }else if(s2=='B')
+    {
+        var b=8;
+    }else if(s2=='C')
+    {
+        var b=7;
+    }else if(s2=='D')
+    {
+        var b=6;
+    }else if(s2=='E')
+    {
+        var b=5;
+    }else if(s2=='')
+    {
+        var b=0;
+    }else
+    {
+        document.getElementById("Err").innerHTML="Enter a Valid Value";
+    }
+
+
+    let s3= document.getElementById("sub3").value;
+    if(s3=='O')
+    {
+        var c=10;
+    }else if(s3=='A')
+    {
+        var c=9;
+    }else if(s3=='B')
+    {
+        var c=8;
+    }else if(s3=='C')
+    {
+        var c=7;
+    }else if(s3=='D')
+    {
+        var c=6;
+    }else if(s3=='E')
+    {
+        var c=5;
+    }else if(s3=='')
+    {
+        var c=0;
+    }else
+    {
+        document.getElementById("Err").innerHTML="Enter a Valid Value";
+    }
+
+    let s4= document.getElementById("sub4").value;
+    if(s4=='O')
+    {
+        var d=10;
+    }else if(s4=='A')
+    {
+        var d=9;
+    }else if(s4=='B')
+    {
+        var d=8;
+    }else if(s4=='C')
+    {
+        var d=7;
+    }else if(s4=='D')
+    {
+        var d=6;
+    }else if(s4=='E')
+    {
+        var d=5;
+    }else if(s4=='')
+    {
+        var d=0;
+    }else
+    {
+        document.getElementById("Err").innerHTML="Enter a Valid Value";
+    }
+
+    let s5= document.getElementById("sub5").value;
+    if(s5=='O')
+    {
+        var e=10;
+    }else if(s5=='A')
+    {
+        var e=9;
+    }else if(s5=='B')
+    {
+        var e=8;
+    }else if(s5=='C')
+    {
+        var e=7;
+    }else if(s5=='D')
+    {
+        var e=6;
+    }else if(s5=='E')
+    {
+        var e=5;
+    }else if(s5=='')
+    {
+        var e=0;
+    }else
+    {
+        document.getElementById("Err").innerHTML="Enter a Valid Value";
+    }
+
+    let s6= document.getElementById("sub6").value;
+    if(s6=='O')
+    {
+        var f=10;
+    }else if(s6=='A')
+    {
+        var f=9;
+    }else if(s6=='B')
+    {
+        var f=8;
+    }else if(s6=='C')
+    {
+        var f=7;
+    }else if(s6=='D')
+    {
+        var f=6;
+    }else if(s6=='E')
+    {
+        var f=5;
+    }else if(s6=='')
+    {
+        var f=0;
+    }else
+    {
+        document.getElementById("Err").innerHTML="Enter a Valid Value";
+    }
+
+    let s7= document.getElementById("sub7").value;
+    if(s7=='O')
+    {
+        var g=10;
+    }else if(s7=='A')
+    {
+        var g=9;
+    }else if(s7=='B')
+    {
+        var g=8;
+    }else if(s7=='C')
+    {
+        var g=7;
+    }else if(s7=='D')
+    {
+        var g=6;
+    }else if(s7=='E')
+    {
+        var g=5;
+    }else if(s7=='')
+    {
+        var g=0;
+    }else
+    {
+        document.getElementById("Err").innerHTML="Enter a Valid Value";
+    }
+
+    let s8= document.getElementById("sub8").value;
+    if(s8=='O')
+    {
+        var h=10;
+    }else if(s8=='A')
+    {
+        var h=9;
+    }else if(s8=='B')
+    {
+        var h=8;
+    }else if(s8=='C')
+    {
+        var h=7;
+    }else if(s8=='D')
+    {
+        var h=6;
+    }else if(s8=='E')
+    {
+        var h=5;
+    }else if(s8=='')
+    {
+        var h=0;
+    }else
+    {
+        document.getElementById("Err").innerHTML="Enter a Valid Value";
+    }
+
+    let s9= document.getElementById("sub9").value;
+    if(s9=='O')
+    {
+        var i=10;
+    }else if(s9=='A')
+    {
+        var i=9;
+    }else if(s9=='B')
+    {
+        var i=8;
+    }else if(s9=='C')
+    {
+        var i=7;
+    }else if(s9=='D')
+    {
+        var i=6;
+    }else if(s9=='E')
+    {
+        var i=5;
+    }else if(s9=='')
+    {
+        var i=0;
+    }else
+    {
+        document.getElementById("Err").innerHTML="Enter a Valid Value";
+    }
+
+    let s10= document.getElementById("sub10").value;
+    if(s10=='O')
+    {
+        var j=10;
+    }else if(s10=='A')
+    {
+        var j=9;
+    }else if(s10=='B')
+    {
+        var j=8;
+    }else if(s10=='C')
+    {
+        var j=7;
+    }else if(s10=='D')
+    {
+        var j=6;
+    }else if(s10=='E')
+    {
+        var j=5;
+    }else if(s10=='')
+    {
+        var j=0;
+    }else
+    {
+        document.getElementById("Err").innerHTML="Enter a Valid Value";
+    }
+    
+
+
+
+    //document.getElementById("grade").innerHTML=x;
+    //credits
+    
+    let c1=parseInt(document.getElementById("cred1").value);
+    let c2=parseInt(document.getElementById("cred2").value);
+    let c3=parseInt(document.getElementById("cred3").value);
+    let c4=parseInt(document.getElementById("cred4").value);
+    let c5=parseInt(document.getElementById("cred5").value);
+    let c6=parseInt(document.getElementById("cred6").value);
+    let c7=parseInt(document.getElementById("cred7").value);
+    let c8=parseInt(document.getElementById("cred8").value);
+    let c9=parseInt(document.getElementById("cred9").value);
+    let c10=parseInt(document.getElementById("cred10").value);
+    if(c10==null || c10==undefined || c10==""){
+        var n=0;
+    }
+
+    console.log(n);
+    n=n??0;
+    console.log(n);
+    console.log(j);
+
+    var y=c1+c2+c3+c4+c5+c6+c7+c8+c9+n;
+    console.log(y);
+   // document.getElementById("grade").innerHTML=y;
+   a=a*c1;
+   b=b*c2;
+   c=c*c3;
+   d=d*c4;
+   e=e*c5;
+   f=f*c6;
+   g=g*c7;
+   h=h*c8;
+   i=i*c9;
+   j=j*n;
+   
+   let x=a+b+c+d+e+f+g+h+i+j;
+   
+   
+ let z=x/y;
+document.getElementById("grade").innerHTML=z; 
+}
